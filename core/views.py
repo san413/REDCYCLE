36f56561eb10ffdf6616c327c05851af22946dca
san413 <sanjayprajapati40458@gmai.com>
2025-05-08 09:01:08 +0530

    modify home.html

--- core/views.py
+++ core/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from education.models import Article
+from faq.models import FAQ
 
 def homepage(request):
-    return render(request, 'core/home.html')
+    articles = Article.objects.all().order_by('-id')[:5]
+    faqs = FAQ.objects.filter(published=True)[:5]
+    return render(request, 'core/home.html', {
+        'articles': articles,
+        'faqs': faqs
+    })
